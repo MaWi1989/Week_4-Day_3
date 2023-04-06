5a9919c7b97bcc80395406fb576c63cb95dee829
Manja Wilson <manjafiebig@aol.com>
2023-04-06 03:00:06 -0400

    second commit

--- HOMEWORK.py
+++ HOMEWORK.py
@@ -2,7 +2,6 @@
 # https://www.codewars.com/kata/59377c53e66267c8f6000027
 
 
-
 # original solution:
 
 # a dictionary for each side (left, right)
@@ -86,7 +85,73 @@ fight = "wwwwwwz"
 # I think this way might be better for space and time complexity, because not only do we only have 1 dictionary to loop through, 
 # but all the parts of this code should just be linear, or constant, and nothing is nested into anyhting else to make it quadratic.
 
-
-
 print(alphabet_war(fight))
 print(alphabet_war2(fight))
+
+
+
+
+# Vowel Count
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+# first approach:
+def get_count(sentence):
+    vowel_count = 0
+    for ch in sentence:
+        if ch in 'aeiou':
+            vowel_count += 1 
+    return vowel_count
+
+
+
+# better approach:
+def get_count2(sentence):
+    count = [sentence.count('a'), sentence.count('e'), sentence.count('i'), sentence.count('o'), sentence.count('u')]
+    result = sum(count)
+    return result
+
+sentence = 'falalalala sweet in and out'
+
+print(get_count(sentence))
+print(get_count2(sentence))
+
+
+# since there are only 5 possible vowels, it might be better to count each one of them, list the counts and then sum up that list, 
+# than to have a for loop with an if statement and a membership test as in the first approach
+
+
+
+
+# cats and shelves
+# https://www.codewars.com/kata/62c93765cef6f10030dfa92b
+# first approach:
+def solution(start, finish):  
+    current_shelf = start
+    moves_count = 0
+    while current_shelf < finish:
+        if finish < (current_shelf + 3):
+            current_shelf += 1
+            moves_count += 1
+        else:
+            current_shelf += 3 
+            moves_count += 1
+      
+    return moves_count
+
+
+# better approach:
+# num of shelves to climb = finish - start
+# devide num of shelves to climb by 3: result = how many '3-step-moves'
+# if remainder: remainder = how many '1-step-moves'
+# add num of moves together for final count
+
+def solution2(start, finish): 
+    steps = finish - start
+    final_count = [(steps // 3), (steps % 3)]
+    return sum(final_count)
+
+
+# second approach has no while loop, no if - else statements, just a couple of equations and a sum(), nothing nested
+
+
+print(solution2(1,35))
